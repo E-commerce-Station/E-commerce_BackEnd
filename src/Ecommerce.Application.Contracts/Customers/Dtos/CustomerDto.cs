dfc393f6fb6438cb88e2e50ffa90cc545b6a9c21
bacnt <bacnt@bkhightech.vn>
2022-12-24 15:49:59 +0700

    feat/recreate-project

---
+++ src/Ecommerce.Application.Contracts/Customers/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Ecommerce.Customers.Dtos
+{
+    [Serializable]
+    public class CustomerDto : FullAuditedEntityDto<Guid>
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public string CCCD { get; set; }
+    }
+}
